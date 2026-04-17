bbecce84851804ca800495ed7a67acdd16b4ed74
Prem Verma <premv9546@gmail.com>
2026-04-17 16:32:09 +0530

    added client (frontend) and updated server

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter, Routes} from "react-router-dom"
+
+
+function Home() {
+  return (
+    <>
+    home page
+    </>
+  )
+}
+
+export default Home
--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+
 app.use("/api/auth",authRouter)
 
 app.listen(port,()=>{
