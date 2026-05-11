3b3acdea7c098e9a9625a72ad3695d9a17167e5f
Prem Kumar Verma <premv9546@gmail.com>
2026-05-12 00:00:27 +0530

    Update auth.service.js

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -23,8 +23,8 @@ class AuthService {
   getCookieOptions() {
     return {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure:true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       path: "/",
     };
@@ -33,8 +33,8 @@ class AuthService {
   getLogoutCookieOptions() {
     return {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure:true,
+      sameSite:"none",
       path: "/",
     };
   }
